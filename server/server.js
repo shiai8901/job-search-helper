9bda2dd80c50f02d2ba2d2c33ef82590e084b433
Ai Shi <shiai8901@gmail.com>
2017-07-05 16:57:55 -0700

    added more dependencies

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const router = express.Router();
+
 
 const port = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + '/dist'));
+
+router.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*")
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	next();
+});
+
+router.get('/', (req, res) => {
+
+	res.send("hello")
+});
 
 app.use('/api', router);
 app.listen(port);
